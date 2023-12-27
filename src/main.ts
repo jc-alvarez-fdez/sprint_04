e4c841f5b7298be0948956f53f2a87c18581f624
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2023-12-27 17:13:09 +0100

    Primer commit Nivell 01 Exercici 01

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+
+const nouAcuditBtn: HTMLElement | null = document.querySelector("#nouAcudit");
+
+// Verifico si el botón existe antes de agregar el evento, para evitar el error
+if (nouAcuditBtn) {
+    // Agregar un evento de clic al botón que llamará a la función obtenerDatos
+    nouAcuditBtn.addEventListener("click", obtenirAcudit);
+}
+
+
+// La función "obtenirAcudit()" devuelve una Promise con el chiste
+function obtenirAcudit() {
+
+    return new Promise(function(resolve, reject) {
+
+        // Definir la URL de la API de chistes
+        const url = "https://icanhazdadjoke.com/";
+
+        // Realizo una solicitud Fetch a la API con el encabezado "Accept" para JSON, según la documentación de la API
+        fetch(url, {
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+
+        .then(respuesta => {
+            // Verifico la respuesta: exito = status code 200
+            if (!respuesta.ok) {
+                throw new Error(`Error a la sol·licitud: ${respuesta.status}`);
+            }
+            // Convierto la respuesta a formato JSON
+            return respuesta.json();
+        })
+        
+        .then(resultado => {
+            // Muestro el resultado en la consola
+            console.log(resultado);
+            // Muestro el resultado en el html
+            mostrarAcudit(resultado);
+            // Resuelvo la "Promise" con el chiste
+            resolve(resultado);
+        })
+
+        .catch(error => {
+            // Manejo de los errores, si los hay
+            console.error(error);
+        });
+    });
+}
+
+function mostrarAcudit(resultado: any) {
+
+    const mostraAcuditSal: HTMLElement | null = document.querySelector("#mostraAcudit");
+
+
+    // Verifico si el elemento existe antes de agregar el evento, para evitar el error (is possibly 'null')
+    if(mostraAcuditSal) {
+        mostraAcuditSal.innerHTML = resultado.joke;
+    }
+}
+// Llamo a obtenirAcudit al cargar la página para obtener y mostrar un chiste inicial
+obtenirAcudit().catch(error => console.error(error));
